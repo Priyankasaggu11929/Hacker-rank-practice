9c9d6be704488e35f2f59f7f57cd8a16cd645c3d
Priyanka Saggu <priyankasggu11929@gmail.com>
2018-08-07 16:14:20 +0530

    Day-3 problem-3

---
+++ Grading_Students.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the gradingStudents function below.
+#
+def gradingStudents(grades):
+    #
+    # Write your code here.
+    #
+    final=[]
+    for i in range(len(grades)):
+        if grades[i]>37:
+            x=grades[i]%5
+            if (5-x)<3:
+                grades[i]+=(5-x)
+                final.append(grades[i])
+            else:
+                final.append(grades[i])
+        else:
+            final.append(grades[i])
+    return final
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    grades = []
+
+    for _ in range(n):
+        grades_item = int(input())
+        grades.append(grades_item)
+    
+    print(grades)
+
+    result = gradingStudents(grades)
+
+    f.write('\n'.join(map(str, result)))
+    f.write('\n')
+
+    f.close()
+
